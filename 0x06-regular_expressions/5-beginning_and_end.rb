498c09b9ad30be33049228907c2e63b64351847d
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-11-15 06:31:28 -0800

    done

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #This is a ruby script exactly match a string that starts with h ends with n and can have any single character in between
-#puts ARGV[0].scan(/h.n/).join
+puts ARGV[0].scan(/h.n/).join
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #This script match only capital letters
-puts ARGV{0}.scan(/[A-Z]/).join
+puts ARGV[0].scan(/[A-Z]/).join
