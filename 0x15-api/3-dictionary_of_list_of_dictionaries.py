32d11a676d0af577e57c9b7b6b3cb20c0fcf8097
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-02-09 05:31:01 -0800

    done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     users = requests.get("https://jsonplaceholder.typicode.com/users").json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-    todoAll = None
+    todoAll = {}
 
     for user in users:
         taskList = []
