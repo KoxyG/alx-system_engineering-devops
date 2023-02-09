46761341ab519dada6058695a861ab7bb930a2e9
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-02-09 04:47:03 -0800

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     user = requests.get("https://jsonplaceholder.typicode.com/users{}"
                         .format(userId))
     username = user.json().get('username')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos', params={"userId": userId})
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
     filename = userId + ".csv"
-    with open(filename, mode='w', newline="") as f:
+    with open(filename, mode='w') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in todos.json():
             if task.get('userId') == int(userId):
